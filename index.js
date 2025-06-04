fcd93a350627f16b0d2433ae6f0e32bc259d2ed1
LJ-Fryer <ljf_aus@hotmail.com>
2025-06-04 17:57:07 +1000

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Testing");
+const triggers = document.querySelectorAll("a");
+const highlight = document.createElement("span");
+
+highlight.classList.add("highlight");
+document.body.append(highlight);
+
+function highlightLink() {
+  const linkCoords = this.getBoundingClientRect();
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY, // adjusts for scrolling Y axis
+    left: linkCoords.left + window.scrollX, // adjusts for scrolling X axis
+  };
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+
+triggers.forEach((a) => a.addEventListener("mouseenter", highlightLink));
